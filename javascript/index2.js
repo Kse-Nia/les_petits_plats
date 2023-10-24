9e6e49f02ebe17af331ef403c41a52f97717adec
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-10-24 22:06:48 +0200

    search and filters, working separately

---
+++ javascript/index2.js
@@ -0,0 +1,189 @@
+import { recipes } from "../data/recipes.js";
+import {
+  getAppliances,
+  getIngredients,
+  getUtensils,
+  createFilterMenu,
+  runSearch,
+} from "./tags.js";
+console.log(recipes);
+
+function createCard(recipes) {
+  const recipesContainer = document.querySelector("#recipes");
+  const folderPictures = "assets/photos/";
+  const filterContainer = document.querySelector(".filters");
+  const searchForm = document.querySelector("#searchbar");
+
+  for (let i = 0; i < recipes.length; i++) {
+    const card = document.createElement("div");
+    card.classList.add("col-12", "col-sm-6", "col-lg-4", "p-3");
+
+    const cardInner = document.createElement("div");
+    cardInner.classList.add("card", "rounded", "h-100");
+    card.appendChild(cardInner);
+
+    // Image
+    const imageContainer = document.createElement("div");
+    imageContainer.classList.add("container-img", "position-relative");
+    cardInner.appendChild(imageContainer);
+
+    const image = document.createElement("img");
+    image.src = folderPictures + recipes[i].image;
+    image.classList.add("card-img", "rounded-top");
+    image.alt = "recipe photo";
+    imageContainer.appendChild(image);
+
+    // Time overlay
+    const timeOverlay = document.createElement("div");
+    timeOverlay.classList.add(
+      "time-overlay",
+      "d-flex",
+      "align-items-center",
+      "justify-content-center",
+      "rounded-pill"
+    );
+    const time = document.createElement("p");
+    time.classList.add("m-0");
+    time.textContent = `${recipes[i].time} min`;
+    timeOverlay.appendChild(time);
+    imageContainer.appendChild(timeOverlay);
+
+    // Card body
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body", "m-2");
+    cardInner.appendChild(cardBody);
+
+    // Title
+    const title = document.createElement("h2");
+    title.classList.add("card-title", "fs-4", "fw-bolder");
+    title.textContent = recipes[i].name;
+    cardBody.appendChild(title);
+
+    // Recipe label
+    const recetteLabel = document.createElement("p");
+
+    recetteLabel.classList.add("recette");
+    recetteLabel.textContent = "RECETTE";
+    cardBody.appendChild(recetteLabel);
+
+    // Description
+    const description = document.createElement("p");
+    description.classList.add("card-text", "text-wrap");
+    description.textContent = recipes[i].description;
+    cardBody.appendChild(description);
+
+    // Truncate description if too long
+    function truncateDescription(text, maxLength) {
+      return text.length > maxLength ? text.substr(0, maxLength) + "..." : text;
+    }
+    description.textContent = truncateDescription(description.textContent, 150);
+
+    // Ingredients
+    const ingredientsContainer = document.createElement("div");
+    ingredientsContainer.classList.add(
+      "container-ingredients",
+      "d-flex",
+      "flex-row",
+      "flex-wrap"
+    );
+    cardBody.appendChild(ingredientsContainer);
+
+    for (let j = 0; j < recipes[i].ingredients.length; j++) {
+      /*    console.log(recipes[i].ingredients[j]); */
+      const ingredient = recipes[i].ingredients[j]; // Get all ingredients
+      const ingredientDiv = document.createElement("div"); // Ingredient div container
+      ingredientDiv.classList.add("col-6");
+      ingredientsContainer.appendChild(ingredientDiv);
+
+      const ingredientName = document.createElement("span");
+      ingredientName.classList.add("card-text", "mb-0", "fw-bolder");
+      ingredientName.textContent = ingredient.ingredient;
+      ingredientDiv.appendChild(ingredientName);
+
+      const ingredientDetails = document.createElement("span");
+      ingredientDetails.classList.add("card-text-quantity", "mb-0");
+      let text = ingredient.quantity ? `: ${ingredient.quantity}` : "";
+      text += ingredient.unit ? ` ${ingredient.unit.slice(0, 2)}` : "";
+      ingredientDetails.textContent = text;
+      ingredientDiv.appendChild(ingredientDetails);
+    }
+
+    recipesContainer.appendChild(card);
+  }
+}
+
+createCard(recipes);
+
+// Search and rendering
+
+// Code pobject array type
+/* function filterRecipes(searchTerm) {
+  return recipes.filter(
+    (recipe) =>
+      recipe.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      recipe.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      recipe.ingredients.some((ingredient) =>
+        ingredient.ingredient.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+  );
+}
+} */
+
+function handleSearch() {
+  const searchForm = document.querySelector("#searchbar"); // Select search form
+  searchForm.addEventListener("input", (e) => {
+    const inputValue = e.target.value;
+    // At least 3 characters input to launch search
+    if (inputValue && inputValue.trim().length >= 3) {
+      const filteredRecipes = filterRecipes(inputValue);
+      renderRecipes(filteredRecipes);
+    } else {
+      renderRecipes(); // Rendering all recipes if empty input
+    }
+  });
+}
+
+function filterRecipes(searchTerm) {
+  const filteredRecipes = [];
+
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+    const nameMatch = recipe.name.toLowerCase().includes(searchTerm);
+    const descriptionMatch = recipe.description
+      .toLowerCase()
+      .includes(searchTerm);
+
+    let ingredientMatch = false;
+    for (let j = 0; j < recipe.ingredients.length; j++) {
+      const ingredient = recipe.ingredients[j];
+      if (ingredient.ingredient.toLowerCase().includes(searchTerm)) {
+        ingredientMatch = true;
+        break;
+      }
+    }
+    if (nameMatch || descriptionMatch || ingredientMatch) {
+      filteredRecipes.push(recipe);
+    }
+  }
+  return filteredRecipes;
+}
+
+filterRecipes();
+
+export function renderRecipes(searchResults = recipes) {
+  const recipesContainer = document.querySelector("#recipes");
+  recipesContainer.innerHTML = "";
+  createCard(searchResults);
+}
+
+handleSearch();
+
+// Get tags
+const uniqueIngredients = getIngredients(recipes);
+const uniqueAppliances = getAppliances(recipes);
+const uniqueUtensils = getUtensils(recipes);
+
+// Add tags to the DOM
+createFilterMenu("Ingredients", uniqueIngredients, renderRecipes);
+createFilterMenu("Appliances", uniqueAppliances, renderRecipes);
+createFilterMenu("Utensils", uniqueUtensils, renderRecipes);
---
+++ javascript/tags.js
@@ -0,0 +1,260 @@
+import { recipes } from "../data/recipes.js";
+
+// Get all types of filter tags
+function getIngredients(recipes) {
+  const ingredients = [];
+  for (let i = 0; i < recipes.length; i++) {
+    for (let j = 0; j < recipes[i].ingredients.length; j++) {
+      if (!ingredients.includes(recipes[i].ingredients[j].ingredient)) {
+        ingredients.push(recipes[i].ingredients[j].ingredient);
+      }
+    }
+  }
+  ingredients.sort();
+  return ingredients;
+}
+
+function getAppliances(recipes) {
+  const appliances = [];
+  for (let i = 0; i < recipes.length; i++) {
+    let applianceData = recipes[i].appliance;
+    if (Array.isArray(applianceData)) {
+      for (let j = 0; j < applianceData.length; j++) {
+        if (!appliances.includes(applianceData[j])) {
+          appliances.push(applianceData[j]);
+        }
+      }
+    } else {
+      if (!appliances.includes(applianceData)) {
+        appliances.push(applianceData);
+      }
+    }
+  }
+  appliances.sort();
+  return appliances;
+}
+
+function getUtensils(recipes) {
+  const utensils = [];
+  for (let i = 0; i < recipes.length; i++) {
+    for (let j = 0; j < recipes[i].ustensils.length; j++) {
+      if (!utensils.includes(recipes[i].ustensils[j])) {
+        utensils.push(recipes[i].ustensils[j]);
+      }
+    }
+  }
+  utensils.sort();
+  return utensils;
+}
+
+// DOM
+function createFilterMenu(categoryName, items, renderRecipes) {
+  const filterContainer = document.querySelector(".filters-container");
+  const selectedTagSection = document.querySelector(".selected-filters");
+  const selectedTagsContainer = document.querySelector(
+    ".selected-tags-container"
+  );
+  selectedTagsContainer.appendChild(selectedTagSection);
+  const categoryWrapper = document.createElement("div");
+  const tagIcon = document.createElement("i");
+
+  categoryWrapper.classList.add(
+    "wrapper-category",
+    "flex",
+    "direction-column",
+    "border",
+    "rounded",
+    "p-2",
+    "m-2",
+    "bg-white"
+  );
+
+  const categoryTitle = document.createElement("div");
+  categoryTitle.classList.add(
+    `category_title-${categoryName.toLowerCase().split(" ").join("-")}`,
+    "m-1"
+  );
+  tagIcon.classList.add(
+    "bi",
+    "bi-chevron-compact-down",
+    "custom-margin",
+    "bi-2x"
+  );
+
+  categoryTitle.textContent = categoryName;
+  categoryTitle.appendChild(tagIcon);
+
+  categoryWrapper.appendChild(categoryTitle);
+
+  const itemsList = document.createElement("div");
+  itemsList.style.display = "none";
+  itemsList.classList.add("dropdown-content", "flex", "direction-column");
+  categoryWrapper.appendChild(itemsList);
+
+  // Search input
+  const searchInput = document.createElement("input");
+  searchInput.setAttribute("type", "text");
+  searchInput.classList.add("search-input");
+  itemsList.appendChild(searchInput);
+
+  // Items list
+  const itemList = document.createElement("div");
+  itemList.classList.add("item-list");
+  itemList.style.backgroundColor = categoryTitle.style.backgroundColor;
+  itemsList.appendChild(itemList);
+
+  items.forEach((item) => {
+    const listItem = document.createElement("div");
+    const title = document.createElement("p");
+    title.textContent = item;
+    listItem.appendChild(title);
+
+    listItem.classList.add("tag");
+
+    // Event
+    listItem.addEventListener("click", function () {
+      this.classList.toggle("selected");
+
+      const selectedTags = {
+        ingredients: Array.from(
+          document
+            .querySelector(".category_title-ingredients")
+            .nextElementSibling.querySelectorAll(".selected")
+        ).map((tag) => tag.textContent),
+        appliances: Array.from(
+          document
+            .querySelector(".category_title-appliances")
+            .nextElementSibling.querySelectorAll(".selected")
+        ).map((tag) => tag.textContent),
+        utensils: Array.from(
+          document
+            .querySelector(".category_title-utensils")
+            .nextElementSibling.querySelectorAll(".selected")
+        ).map((tag) => tag.textContent),
+      };
+
+      const searchResults = runSearch(recipes, selectedTags);
+      renderRecipes(searchResults); // Rendering recipes using search results
+
+      if (this.classList.contains("selected")) {
+        createSelectedTagButton(this.textContent, selectedTagsContainer);
+      } else {
+        removeSelectedTagButton(this.textContent, selectedTagsContainer);
+      }
+    });
+
+    itemList.appendChild(listItem);
+  });
+
+  // Event listener for search input in tags
+  searchInput.addEventListener("input", () => {
+    const searchValue = searchInput.value.toLowerCase();
+    const listItems = itemList.getElementsByClassName("tag");
+
+    // Search with at least 3 letters
+    if (searchValue.length >= 3) {
+      for (let i = 0; i < listItems.length; i++) {
+        const textValue = listItems[i].textContent.toLowerCase();
+        if (textValue.includes(searchValue)) {
+          listItems[i].style.display = "block";
+        } else {
+          listItems[i].style.display = "none";
+        }
+      }
+    } else {
+      for (let i = 0; i < listItems.length; i++) {
+        listItems[i].style.display = "block";
+      }
+    }
+  });
+
+  // Selecting filter tags
+  function createSelectedTagButton(tagName, container) {
+    const tagButton = document.createElement("button");
+
+    // Create a span element to hold the tag name
+    const span = document.createElement("span");
+    span.textContent = tagName;
+    tagButton.appendChild(span);
+
+    // Add remove icon
+    const icon = document.createElement("i");
+    icon.classList.add("bi", "bi-x", "tag_close-icon");
+    tagButton.appendChild(icon);
+
+    tagButton.classList.add("selected-tag-button");
+
+    // Event listener for click on icon
+    icon.addEventListener("click", () => {
+      container.removeChild(tagButton);
+    });
+
+    container.appendChild(tagButton);
+  }
+
+  function removeSelectedTagButton(tagName, container) {
+    const existingButtons = container.querySelectorAll(".selected-tag-button");
+    existingButtons.forEach((button) => {
+      if (button.textContent === tagName) {
+        container.removeChild(button);
+      }
+    });
+  }
+  // Open Filter on click
+  categoryTitle.addEventListener("click", () => {
+    tagIcon.classList.toggle("rotate-icon"); // add rotate class
+    itemsList.style.display =
+      itemsList.style.display === "none" ? "block" : "none";
+    console.log("Rotate toggled");
+    console.log(tagIcon);
+  });
+
+  categoryWrapper.appendChild(itemsList);
+  filterContainer.appendChild(categoryWrapper);
+
+  categoryWrapper.appendChild(itemsList);
+  filterContainer.appendChild(categoryWrapper);
+}
+
+// Search
+function runSearch(recipes, selectedTags) {
+  return recipes.filter((recipe) => {
+    const ingredientsMatch = selectedTags.ingredients.every((tag) =>
+      recipe.ingredients.some(
+        (ingredient) =>
+          ingredient.ingredient.toLowerCase() === tag.toLowerCase()
+      )
+    );
+    const appliancesMatch = selectedTags.appliances.every(
+      (tag) => recipe.appliance.toLowerCase() === tag.toLowerCase()
+    );
+    const utensilsMatch = selectedTags.utensils.every((tag) =>
+      recipe.utensils.some(
+        (utensil) => utensil.toLowerCase() === tag.toLowerCase()
+      )
+    );
+    return ingredientsMatch && appliancesMatch && utensilsMatch;
+  });
+}
+
+export {
+  getIngredients,
+  getAppliances,
+  getUtensils,
+  createFilterMenu,
+  runSearch,
+};
+
+function initButtonClickEvent() {
+  const button = document.querySelector("#myButton");
+  // Verify if button exists
+  if (button) {
+    button.addEventListener("click", () => {
+      console.log("vous avez cliqué sur le bouton !");
+    });
+  } else {
+    console.log("bouton n'existe pas");
+  }
+}
+
+initButtonClickEvent();
