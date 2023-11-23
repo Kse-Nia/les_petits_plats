c737e7479c3b33d1efd8e8c0e54aa31dcec5ead3
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-11-23 18:02:06 +0100

    clean code - SOLID for cards creation

--- javascript/index.js
+++ javascript/index.js
@@ -7,103 +7,125 @@ import {
 } from "./tags.js";
 import { getSelectedTags } from "./manageSelectedTags.js";
 
+// Global variables
+const recipesContainer = document.querySelector("#recipes");
+const folderPictures = "assets/photos/";
+
+function createCardElements(recipe) {
+  const card = document.createElement("div");
+  card.classList.add("col-12", "col-sm-6", "col-lg-4", "p-3");
+
+  const cardInner = document.createElement("div");
+  cardInner.classList.add("card", "rounded", "h-100");
+  card.appendChild(cardInner);
+
+  addImageToCard(cardInner, recipe);
+  addCardBody(cardInner, recipe);
+
+  return card;
+}
+// Create Image
+function addImageToCard(cardInner, recipe) {
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("container-img", "position-relative");
+  cardInner.appendChild(imageContainer);
+
+  const image = document.createElement("img");
+  image.src = folderPictures + recipe.image;
+  image.classList.add("card-img", "rounded-top");
+  image.alt = "recipe photo";
+  imageContainer.appendChild(image);
+
+  addTimeOverlay(imageContainer, recipe);
+}
+// Create time
+function addTimeOverlay(imageContainer, recipe) {
+  const timeOverlay = document.createElement("div");
+  timeOverlay.classList.add(
+    "time-overlay",
+    "d-flex",
+    "align-items-center",
+    "justify-content-center",
+    "rounded-pill"
+  );
+  const time = document.createElement("p");
+  time.classList.add("m-0");
+  time.textContent = `${recipe.time} min`;
+  timeOverlay.appendChild(time);
+  imageContainer.appendChild(timeOverlay);
+}
+
+// Connect childs to card body element
+function addCardBody(cardInner, recipe) {
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body", "m-2");
+  cardInner.appendChild(cardBody);
+
+  const title = document.createElement("h2");
+  title.classList.add("card-title", "fs-4", "fw-bolder");
+  title.textContent = recipe.name;
+  cardBody.appendChild(title);
+
+  const recetteLabel = document.createElement("p");
+  recetteLabel.classList.add("recette");
+  recetteLabel.textContent = "RECETTE";
+  cardBody.appendChild(recetteLabel);
+
+  const description = addDescription(recipe.description);
+  cardBody.appendChild(description);
+
+  addIngredients(cardBody, recipe.ingredients);
+}
+
+// Create description
+function addDescription(text) {
+  const description = document.createElement("p");
+  description.classList.add("card-text", "text-wrap");
+  description.textContent = truncateDescription(text, 150);
+  return description;
+}
+
+// Create ingredients
+function addIngredients(cardBody, ingredients) {
+  const ingredientsContainer = document.createElement("div");
+  ingredientsContainer.classList.add(
+    "container-ingredients",
+    "d-flex",
+    "flex-row",
+    "flex-wrap"
+  );
+  cardBody.appendChild(ingredientsContainer);
+
+  for (let i = 0; i < ingredients.length; i++) {
+    const ingredient = ingredients[i];
+    const ingredientDiv = document.createElement("div");
+    ingredientDiv.classList.add("col-6");
+    ingredientsContainer.appendChild(ingredientDiv);
+
+    const ingredientName = document.createElement("span");
+    ingredientName.classList.add("card-text", "mb-0", "fw-bolder");
+    ingredientName.textContent = ingredient.ingredient;
+    ingredientDiv.appendChild(ingredientName);
+
+    const ingredientDetails = document.createElement("span");
+    ingredientDetails.classList.add("card-text-quantity", "mb-0");
+    let text = ingredient.quantity ? `: ${ingredient.quantity}` : "";
+    text += ingredient.unit ? ` ${ingredient.unit.slice(0, 2)}` : "";
+    ingredientDetails.textContent = text;
+    ingredientDiv.appendChild(ingredientDetails);
+  }
+}
+
+function truncateDescription(text, maxLength) {
+  return text.length > maxLength ? text.substr(0, maxLength) + "..." : text;
+}
+
+// Generate cards
 function createCard(recipes) {
-  const recipesContainer = document.querySelector("#recipes");
-  const folderPictures = "assets/photos/";
+  const recipesLength = recipes.length;
 
-  for (let i = 0; i < recipes.length; i++) {
-    const card = document.createElement("div");
-    card.classList.add("col-12", "col-sm-6", "col-lg-4", "p-3");
-
-    const cardInner = document.createElement("div");
-    cardInner.classList.add("card", "rounded", "h-100");
-    card.appendChild(cardInner);
-
-    // Image
-    const imageContainer = document.createElement("div");
-    imageContainer.classList.add("container-img", "position-relative");
-    cardInner.appendChild(imageContainer);
-
-    const image = document.createElement("img");
-    image.src = folderPictures + recipes[i].image;
-    image.classList.add("card-img", "rounded-top");
-    image.alt = "recipe photo";
-    imageContainer.appendChild(image);
-
-    // Time overlay
-    const timeOverlay = document.createElement("div");
-    timeOverlay.classList.add(
-      "time-overlay",
-      "d-flex",
-      "align-items-center",
-      "justify-content-center",
-      "rounded-pill"
-    );
-    const time = document.createElement("p");
-    time.classList.add("m-0");
-    time.textContent = `${recipes[i].time} min`;
-    timeOverlay.appendChild(time);
-    imageContainer.appendChild(timeOverlay);
-
-    // Card body
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body", "m-2");
-    cardInner.appendChild(cardBody);
-
-    // Title
-    const title = document.createElement("h2");
-    title.classList.add("card-title", "fs-4", "fw-bolder");
-    title.textContent = recipes[i].name;
-    cardBody.appendChild(title);
-
-    // Recipe label
-    const recetteLabel = document.createElement("p");
-
-    recetteLabel.classList.add("recette");
-    recetteLabel.textContent = "RECETTE";
-    cardBody.appendChild(recetteLabel);
-
-    // Description
-    const description = document.createElement("p");
-    description.classList.add("card-text", "text-wrap");
-    description.textContent = recipes[i].description;
-    cardBody.appendChild(description);
-
-    // Truncate description if too long
-    function truncateDescription(text, maxLength) {
-      return text.length > maxLength ? text.substr(0, maxLength) + "..." : text;
-    }
-    description.textContent = truncateDescription(description.textContent, 150);
-
-    // Ingredients
-    const ingredientsContainer = document.createElement("div");
-    ingredientsContainer.classList.add(
-      "container-ingredients",
-      "d-flex",
-      "flex-row",
-      "flex-wrap"
-    );
-    cardBody.appendChild(ingredientsContainer);
-
-    for (let j = 0; j < recipes[i].ingredients.length; j++) {
-      const ingredient = recipes[i].ingredients[j]; // Get all ingredients
-      const ingredientDiv = document.createElement("div"); // Ingredient div container
-      ingredientDiv.classList.add("col-6");
-      ingredientsContainer.appendChild(ingredientDiv);
-
-      const ingredientName = document.createElement("span");
-      ingredientName.classList.add("card-text", "mb-0", "fw-bolder");
-      ingredientName.textContent = ingredient.ingredient;
-      ingredientDiv.appendChild(ingredientName);
-
-      const ingredientDetails = document.createElement("span");
-      ingredientDetails.classList.add("card-text-quantity", "mb-0");
-      let text = ingredient.quantity ? `: ${ingredient.quantity}` : "";
-      text += ingredient.unit ? ` ${ingredient.unit.slice(0, 2)}` : "";
-      ingredientDetails.textContent = text;
-      ingredientDiv.appendChild(ingredientDetails);
-    }
-
+  for (let i = 0; i < recipesLength; i++) {
+    const card = createCardElements(recipes[i]);
     recipesContainer.appendChild(card);
   }
 }
@@ -180,6 +202,12 @@ export function renderRecipes(filteredRecipes, searchInput) {
   if (filteredRecipes.length === 0) {
     // No matched recipes -> display message for no result
     const noRecipesMessage = document.createElement("p");
+    noRecipesMessage.classList.add(
+      "p-1",
+      "m-5",
+      "text-center",
+      "recipes_error-message"
+    );
     noRecipesMessage.textContent = `Aucune recette ne contient '${searchInput}', vous pouvez chercher "tarte aux pommes", "poisson", etc.`;
     recipesContainer.appendChild(noRecipesMessage);
   } else {
