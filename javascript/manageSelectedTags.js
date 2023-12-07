d564a52b08291b70f462358c2c9484eb2e602eef
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-12-07 11:24:47 +0100

    update to for loop methode

--- javascript/manageSelectedTags.js
+++ javascript/manageSelectedTags.js
@@ -13,15 +13,33 @@ function determinedCategory(categoryName) {
 }
 
 function addTag(category, tag) {
-  if (!selectedTags[category].includes(tag)) {
-    selectedTags[category].push(tag);
+  let foundTag = false;
+  for (let i = 0; i < selectedTags[category].length; i++) {
+    if (selectedTags[category][i] === tag) {
+      foundTag = true;
+      break;
+    }
+  }
+  if (!foundTag) {
+    selectedTags[category][selectedTags[category].length] = tag;
   }
 }
 
 function removeTag(category, tag) {
-  const index = selectedTags[category].indexOf(tag);
+  let index = -1;
+  // Find tag index
+  for (let i = 0; i < selectedTags[category].length; i++) {
+    if (selectedTags[category][i] === tag) {
+      index = i;
+      break;
+    }
+  }
+  // Remove tag
   if (index > -1) {
-    selectedTags[category].splice(index, 1);
+    for (let i = index; i < selectedTags[category].length - 1; i++) {
+      selectedTags[category][i] = selectedTags[category][i + 1];
+    }
+    selectedTags[category].length--;
   }
 }
 
